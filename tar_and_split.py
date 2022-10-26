223a3f5a89bf3aaf53b3dc45203004e328ef7a6d
Lanqing Yuan <yuanlq@uchicago.edu>
2022-10-26 13:01:46 -0500

    not compressing anymore to speed up

--- tar_and_split.py
+++ tar_and_split.py
@@ -17,9 +17,9 @@ piname = absolute_dir.split("/")[2]
 os.system("mkdir /%s/%s/%s_relocated/" % (disk, piname, usrname))
 temp_tar_dir = "/%s/%s/%s_relocated/" % (disk, piname, usrname) + usrname
 # look like '/dali/lgrandi/yuanlq_relocated/yuanlq.tar.gz'
-temp_tar_dir += ".tar.gz"
+temp_tar_dir += ".tar"
 
-cmd_tar = "tar -czf %s %s" % (temp_tar_dir, absolute_dir)
+cmd_tar = "tar -cf %s %s" % (temp_tar_dir, absolute_dir)
 print("Command for tar: ", cmd_tar)
 # Hardcoded to split at 100G each now...
 cmd_split = 'split -b 100G %s "%s"' % (temp_tar_dir, temp_tar_dir + ".part")
@@ -29,9 +29,9 @@ os.system(cmd_tar)
 print("Finished tarring. Now splitting them into 100G chunks.")
 os.system(cmd_split)
 
-print("Already tarred and split. Now we delete the .tar.gz file!")
+print("Already tarred and split. Now we delete the .tar file!")
 os.system("rm %s" % (temp_tar_dir))
-print("Delted .tar.gz file!")
+print("Delted .tar file!")
 
 print(" ")
 print("=============")
