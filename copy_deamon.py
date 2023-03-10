7751673fd7c8d62f824733c738fe931355cf1e4b
Lanqing Yuan <yuanlq@uchicago.edu>
2023-03-10 15:54:31 -0600

    bug fix in copy_deamon

--- copy_deamon.py
+++ copy_deamon.py
@@ -1,19 +1,19 @@
 import cutax
 import straxen
 import strax
+import time
 
 st = cutax.contexts.xenonnt_online(output_folder='/project2/lgrandi/xenonnt/processed')
 
-dtypes = ["peak_basics", "peak_positions_mlp", "lone_hits", "pulse_counts", "merged_s2s", 
-          "peaklet_classification", "peaklet_classification_he", "veto_regions", "pulse_counts_he", 
-          "peaklets_he","led_calibration","hitlets_mv","hitlets_nv", "event_basics", "events", 
-          "event_info",  "peak_proximity", "peak_positions", "peak_positions_cnn", "peak_positions_gcn", 
-          "events_mv", "events_nv", "afterpulses"]
+dtypes = ["peak_basics", "peak_positions_mlp", 
+          "peaklet_classification", "peaklet_classification_he", "veto_regions", 
+          "event_basics", "events", "event_info",  "peak_proximity", "peak_positions", 
+          "peak_positions_cnn", "peak_positions_gcn", "events_mv", "events_nv"] 
 
 while True:
     # Get online
     st = cutax.contexts.xenonnt_online(output_folder='/project2/lgrandi/xenonnt/processed')
-    runs = st.select_runs(available='event_basics').name[:5]
+    runs = st.select_runs(available='event_basics').name[:10]
     for run in strax.utils.tqdm(runs):
         for dtype in dtypes:
             try:
@@ -22,5 +22,5 @@ while True:
                 print(dtype+' for run '+run+' is not available or already exists in destination.')
 
     # sleep for half an hour.        
-    sleep(1800)
+    time.sleep(1800)
 
