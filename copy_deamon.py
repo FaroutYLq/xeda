22d67c149149276ce59c15571e83ebf8c5def466
Lanqing Yuan <yuanlq@uchicago.edu>
2023-03-24 15:29:31 -0500

    updated dtype and added offline getting

--- copy_deamon.py
+++ copy_deamon.py
@@ -5,10 +5,11 @@ import time
 
 st = cutax.contexts.xenonnt_online(output_folder='/project2/lgrandi/xenonnt/processed')
 
-dtypes = ["peak_basics", "peak_positions_mlp", 
-          "peaklet_classification", "peaklet_classification_he", "veto_regions", 
-          "event_basics", "events", "event_info",  "peak_proximity", "peak_positions", 
+dtypes = ["peak_basics",  
+          "peaklet_classification", "peaklet_classification_he", "veto_regions", "event_pattern_fit", "event_w_bayes_class", 
+          "event_basics", "events", "event_info",  "peak_proximity", "peak_positions", "peak_positions_mlp", 
           "peak_positions_cnn", "peak_positions_gcn", "events_mv", "events_nv"] 
+
 
 while True:
     # Get online
@@ -20,7 +21,15 @@ while True:
                 st.copy_to_frontend(run, dtype)
             except:
                 print(dtype+' for run '+run+' is not available or already exists in destination.')
-
+    # Get offline radon
+    st = cutax.contexts.xenonnt_offline(output_folder='/project2/lgrandi/xenonnt/processed')
+    runs = st.select_runs(run_mode=('tpc_radon', 'tpc_radon_hev'), available='event_basics')
+    for run in strax.utils.tqdm(runs):
+        for dtype in dtypes:
+            try:
+                st.copy_to_frontend(run, dtype)
+            except:
+                print(dtype+' for run '+run+' is not available or already exists in destination.')
+                
     # sleep for half an hour.        
     time.sleep(1800)
-
