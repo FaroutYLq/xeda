a3ac614fbd93fcf57c529ce16aded996fe0fad30
Lanqing Yuan <yuanlq@uchicago.edu>
2023-04-23 13:04:19 -0500

    switched to singularity

--- submit_xeda.sh
+++ submit_xeda.sh
@@ -1,3 +1,4 @@
-. /cvmfs/xenon.opensciencegrid.org/releases/nT/development/setup.sh
+module load singularity
+singularity shell --bind /project2 --bind /project/lgrandi/ --bind /scratch/midway2/$USER --bind /project2/lgrandi/xenonnt/dali:/dali /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
 python /home/yuanlq/software/xeda/batch_job.py
-conda deactivate
+exit
