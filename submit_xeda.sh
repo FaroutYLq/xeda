0b259f336b82f4780683f61402e30227b7d75ab1
Lanqing Yuan <yuanlq@uchicago.edu>
2023-04-25 14:48:49 -0500

    updated directory binding for /project

--- submit_xeda.sh
+++ submit_xeda.sh
@@ -1,4 +1,4 @@
 module load singularity
-singularity shell --bind /project2 --bind /project/lgrandi/ --bind /scratch/midway2/$USER --bind /project2/lgrandi/xenonnt/dali:/dali /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
+singularity shell --bind /project2 --bind /project --bind /scratch/midway2/$USER --bind /project2/lgrandi/xenonnt/dali:/dali /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
 python /home/yuanlq/software/xeda/batch_job.py
 exit
