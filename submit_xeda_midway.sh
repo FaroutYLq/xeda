a189a5e7ab89bb4313ba28dab9c856211c6bb803
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-02 21:50:29 -0500

    added scratch

--- submit_xeda_midway.sh
+++ submit_xeda_midway.sh
@@ -1,4 +1,4 @@
 module load singularity
-singularity shell --bind /project2 --bind /project /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
+singularity shell --bind /project2 --bind /project --bind /scratch/midway2/$USER --bind /scratch/midway3/$USER /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
 python /home/yuanlq/software/xeda/batch_job.py midway
 exit
