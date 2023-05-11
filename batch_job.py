14c93e38b9fd87a5385bd5c36c7a85e6ba7a5a2e
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-11 14:46:16 -0500

    bug fixed

--- batch_job.py
+++ batch_job.py
@@ -16,7 +16,7 @@ TO_SCAN = {
     'midway2': ["/project2/lgrandi/"],
     "midway3": ["/project/lgrandi/"]
     }
-QOS = {"/dali/lgrandi/": "dali", "/project2/lgrandi/": "xenon1t", "/project/lgrandi/": "lgrandi"}
+QOS = {"/dali/lgrandi/": "dali", "/project2/lgrandi/": "broadwl", "/project/lgrandi/": "lgrandi"}
 PARTITION = {"/dali/lgrandi/": "dali", "/project2/lgrandi/": "broadwl", "/project/lgrandi/": "lgrandi"}
 OUTPUT_DIR = {
     "/dali/lgrandi/": "/dali/lgrandi/yuanlq/shared/disk_usage/",
