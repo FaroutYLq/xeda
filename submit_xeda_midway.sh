a302399922e34282c082afcaecc793c602b20691
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-10 23:27:26 -0500

    changed permission

--- submit_xeda_midway.sh
+++ submit_xeda_midway.sh
@@ -1,4 +1,3 @@
-module load singularity
-singularity shell --bind /project2 --bind /project --bind /scratch/midway2/$USER --bind /scratch/midway3/$USER /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
+. /cvmfs/xenon.opensciencegrid.org/releases/nT/development/setup.sh
 python /home/yuanlq/software/xeda/batch_job.py midway
 exit
