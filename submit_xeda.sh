1472d890fdc0711ceee657c570e6131506d69053
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-11 01:22:21 -0500

    Delete submit_xeda.sh

--- submit_xeda.sh
+++
@@ -1,4 +0,0 @@
-module load singularity
-singularity shell --bind /project2 --bind /project --bind /scratch/midway2/$USER --bind /project2/lgrandi/xenonnt/dali:/dali /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
-python /home/yuanlq/software/xeda/batch_job.py
-exit
