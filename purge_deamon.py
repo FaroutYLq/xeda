87c561a97044d5069fbbbf84c6807e6091ee1b7b
Lanqing Yuan <yuanlq@uchicago.edu>
2023-06-08 17:06:04 -0500

    removed _rucio_local_path

--- purge_deamon.py
+++ purge_deamon.py
@@ -5,7 +5,7 @@ import os
 
 straxen.print_versions()
 
-dtypes = ["peak_basics", "raw_records_aqmon","distinct_channels", "veto_proximity", "afterpulses",
+dtypes = ["peak_basics", "raw_records_aqmon","distinct_channels", "veto_proximity", "afterpulses", "veto_intervals",
           "peaklet_classification", "peaklet_classification_he", "veto_regions", "event_pattern_fit", "event_w_bayes_class", 
           "event_basics", "events", "event_info",  "peak_proximity", "peak_positions", "peak_positions_mlp", 
           "peak_positions_cnn", "peak_positions_gcn", "events_mv", "events_nv", "event_positions_nv"] 
@@ -14,7 +14,7 @@ basedir = '/project2/lgrandi/xenonnt/processed/'
 # Context to copy to frontend
 st = cutax.contexts.xenonnt_offline(output_folder='/project2/lgrandi/xenonnt/processed')
 # Context to test loadability
-st_midway = cutax.contexts.xenonnt_offline(_rucio_local_path='/project/lgrandi/rucio', include_rucio_local = True)
+st_midway = cutax.contexts.xenonnt_offline(_rucio_path='/project/lgrandi/rucio')
 st_midway.storage.append(strax.DataDirectory("/project2/lgrandi/xenonnt/processed/", readonly=True))
 
 # Runs available on DaLI
