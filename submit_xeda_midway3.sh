e7b73036c55505b4cdd5568b0c4eb12c32a1f15d
Lanqing Yuan <yuanlq@uchicago.edu>
2023-07-04 18:18:09 -0500

    removed midway3 specific

--- submit_xeda_midway3.sh
+++ submit_xeda_midway3.sh
@@ -1,3 +1,3 @@
 module load singularity
-singularity shell --contain --bind /home/yuanlq --bind /project2 --bind /project/ --bind /scratch/midway2/yuanlq --bind /scratch/midway3/yuanlq /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
+singularity shell --bind /home/yuanlq --bind /project2 --bind /project/ --bind /scratch/midway2/yuanlq --bind /scratch/midway3/yuanlq /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg
 python /home/yuanlq/software/xeda/batch_job.py midway3
