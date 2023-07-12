f43e338cdb69af0abf9ffedf06d76bf584c05e67
Lanqing Yuan <yuanlq@uchicago.edu>
2023-07-12 17:47:48 -0500

    title

--- xeda/utilities.py
+++ xeda/utilities.py
@@ -249,17 +249,21 @@ def check_by_mode(rules, title=None, graph=True, dpi=100, max_n_modes=8, wiki=Tr
 
     if wiki:
         print("<table>")
+        print("^%s^^^^"%(title))
         print("^ Mode ^ Size [TB] ^ Count ^ Percentile ^")
         for i in range(n_unique_modes):
             print("| ''{}'' | {:.2f} | {} | {} |".format(unique_modes[-i-1], 
                                                          sizes_tb[-i-1], 
                                                          counts[-i-1],
                                                          percentiles[-i-1]))
+        print("| **Total** | {:.2f} | {} | 100 |".format(sizes_tb.sum(),
+                                                         counts.sum()))
+        
         print("<caption>")
         print(title)
         print("/caption")
         print("</table>")
-        
+
     if graph:
         plt.figure(dpi=dpi)
         plt.bar(unique_modes[-max_n_modes:], sizes_tb[-max_n_modes:])
